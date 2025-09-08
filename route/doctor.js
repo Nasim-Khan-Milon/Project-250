742f91aa33c004b3f5b80c0682affb826cc0dc47
Nasim-Khan_Milon <nasmkhanmilon@gmail.com>
2025-09-09 00:02:51 +0600

    Add other doctor routes

--- route/doctor.js
+++ route/doctor.js
@@ -27,4 +27,51 @@ router.delete("/availability/:id", async (req, res) => {
     res.redirect("/doctor/dashboard/availability");
 });
 
+//Appointment List Route
+router.get("/appointment", (req, res) => {
+    res.render("doctor/doctor-appointments");
+});
+
+//Pending Appointment List Route
+router.get("/pendingappointment", (req, res) => {
+    res.render("doctor/pending-appointment");
+});
+
+//Previously Viewed Patient List Route
+router.get("/patients", (req, res) => {
+    res.render("doctor/patients");
+});
+
+//Previously Viewed Patient Details Route
+router.get("/patients/patient-details", (req, res) => {
+    res.render("doctor/patient-details");
+});
+
+//Dotor Profile Route
+router.get("/profile", (req, res) => {
+    res.render("doctor/profile");
+});
+
+//Dotor Profile Edit Route
+router.get("/profile/editProfile", (req, res) => {
+    res.render("doctor/edit-profile");
+});
+
+//Log In Route
+router.get("/login", (req, res) => {
+    res.render("doctor/login");
+});
+
+//Sign Up Route
+router.get("/signup", (req, res) => {
+    res.render("doctor/signup");
+});
+
+//Forgot Password Route
+router.get("/forgotPassword", (req, res) => {
+    res.render("doctor/forgot-password");
+});
+
+
+
 module.exports = router;
